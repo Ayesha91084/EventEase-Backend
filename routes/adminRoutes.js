b7bd6331297ed469f8f62630e704fd4de10799c7
ayesha <ayeshabibi828gmail.com>
2026-05-02 17:20:26 +0500

    route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -10,12 +10,3 @@ router.delete('/user/:id', protect, admin, deleteUser);
 
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-const { getDashboardSummary } = require('../controllers/adminController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-// Dashboard summary sirf login admin dekh sakta hai
-router.get('/summary', protect, admin, getDashboardSummary);
-
-module.exports = router;
