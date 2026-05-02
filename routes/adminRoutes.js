160a8ad7f12ad0d9445b0ae750fe8954ae8cf8f6
ayesha <ayeshabibi828gmail.com>
2026-05-02 17:23:26 +0500

    router

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers, deleteUser } = require('../controllers/adminController');
 const { protect, admin } = require('../middleware/authMiddleware');
-
+const User = require('../models/User');
 // Dono middleware (protect aur admin) lagana zaroori hain
 router.get('/users', protect, admin, getAllUsers);
 router.delete('/user/:id', protect, admin, deleteUser);
+
 
 
 
