f2dc4c59dbb2a9fe7e75a2543d42b443eef1d11a
ayesha <ayeshabibi828gmail.com>
2026-08-14 18:44:06 +0500

    server

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Forces Node to use Google DNS for MongoDB
+
+
+
+
 const express = require('express');
 
 const cors = require('cors');
